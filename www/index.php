bff4c09ce2153c286d2f91c4d7766e373f55f823
Jiří Zapletal <zapletal@wakers.cz>
2020-07-09 13:58:48 +0200

    Added coming soon LP

---
+++ www/index.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en" class="h-100">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Powerful marketing abilities | Result oriented thinking | Advanced strategies | Friendly pricing | Webdesign, UX, PPC, CRO that boosts Your business.">
+    <title>STRATEGIO digital - Get more customers, orders, leads or interests</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700&display=swap" rel="stylesheet">
+
+    <style type="text/css">
+        body {
+            font-family: 'Montserrat', sans-serif;
+        }
+        @media (max-width: 728px) {
+            .zoom {
+                zoom: .45;
+            }
+        }
+    </style>
+</head>
+<body class="h-100 d-flex justify-content-center align-items-center text-right">
+<div class="zoom text-left px-5">
+    <div>
+        <h1 class="text-upper font-weight-bold display-1" style="line-height: 1;  color: #00aeef">
+            STRATEGIO<span style="font-size: 16px; font-weight: bold">.digital</span>
+        </h1>
+
+        <p style="color: #5f5f5f; font-size: 60px; line-height: 1.1">
+            powerful marketing abilities
+        </p>
+
+        <p class="mt-5 pt-5 h4" style="color: #5f5f5f">
+            Get more customers, online orders, leads or interests.
+        </p>
+    </div>
+
+    <div class="d-flex justify-content-start mt-3">
+        <a href="mailto:get@strategio.digital" target="_blank" class="mr-3">
+            get@strategio.digital
+        </a>
+
+        <a href="tel:+420606091125" target="_blank">
+            +420 606 091 125
+        </a>
+    </div>
+</div>
+</body>
+</html>
