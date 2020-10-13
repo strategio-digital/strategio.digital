33d4b2a8255c97295488e1e21027f7663e2a003d
Jiří Zapletal <zapletal@wakers.cz>
2020-10-13 22:37:35 +0200

    Upgrade to PHP 8 & refactored CSS

--- www/index.php
+++ www/index.php
@@ -6,44 +6,96 @@
     <meta name="description" content="Powerful marketing abilities | Result oriented thinking | Advanced strategies | Friendly pricing | Webdesign, UX, PPC, CRO that boosts Your business.">
     <title>STRATEGIO digital - Get more customers, orders, leads or interests</title>
 
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <!--link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous"-->
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700&display=swap" rel="stylesheet">
 
     <style type="text/css">
+        html,
         body {
+            margin: 0;
+            height: 100%;
             font-family: 'Montserrat', sans-serif;
+			background: #fff;
+			background: linear-gradient(135deg, #fff 0%, #efefef 100%);
         }
-        @media (max-width: 728px) {
-            .zoom {
-                zoom: .45;
+
+        .container {
+            height: 100%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            padding: 0 1rem;
+        }
+
+        .title {
+            font-size: 6rem;
+            font-weight: bold;
+			line-height: 1;
+            color: #00aeef;
+            margin-bottom: .5rem;
+        }
+
+        p {
+			color: #6b6b6b;
+            line-height: 1;
+            margin: 0;
+        }
+
+        .sub-title {
+            font-size: 3.5rem;
+            margin-bottom: 5rem;
+        }
+
+        .motto {
+            font-size: 1.5rem;
+            margin-bottom: 1rem;
+        }
+
+        .contact a {
+            color: #007bff;
+            margin-right: 1rem;
+        }
+
+		@media (max-width: 728px) {
+            .title {
+                font-size: 2.5rem;
             }
-        }
+            .sub-title {
+                font-size: 1.3rem;
+				margin-bottom: 3rem;
+            }
+			.motto {
+				font-size: 1.1rem;
+			}
+			.contact a {
+				margin-right: 0;
+                display: block;
+                margin-bottom: .5rem;
+			}
+		}
     </style>
 </head>
-<body class="h-100 d-flex justify-content-center align-items-center text-right">
-<div class="zoom text-left px-5">
+<body>
+<div class="container">
     <div>
-        <h1 class="text-upper font-weight-bold display-1" style="line-height: 1;  color: #00aeef">
-            STRATEGIO<span style="font-size: 16px; font-weight: bold">.digital</span>
-        </h1>
+        <div>
+            <h1 class="title" style="">
+                STRATEGIO<span style="font-size: 16px; font-weight: bold">.digital</span>
+            </h1>
 
-        <p style="color: #5f5f5f; font-size: 60px; line-height: 1.1">
-            powerful marketing abilities
-        </p>
+            <p class="sub-title">
+                powerful marketing abilities
+            </p>
 
-        <p class="mt-5 pt-5 h4" style="color: #5f5f5f">
-            Get more customers, online orders, leads or interests.
-        </p>
-    </div>
+            <p class="motto">
+                Get more customers, online orders, leads or interests.
+            </p>
+        </div>
 
-    <div class="d-flex justify-content-start mt-3">
-        <a href="mailto:get@strategio.digital" target="_blank" class="mr-3">
-            get@strategio.digital
-        </a>
-
-        <a href="tel:+420606091125" target="_blank">
-            +420 606 091 125
-        </a>
+        <div class="contact">
+            <a href="mailto:get@strategio.digital" target="_blank">get@strategio.digital</a>
+            <a href="tel:+420606091125" target="_blank">+420 606 091 125</a>
+        </div>
     </div>
 </div>
 </body>
